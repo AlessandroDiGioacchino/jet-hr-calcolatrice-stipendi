28882de0ef936085f87dc4ce85bf6028d5aba2d9
Alessandro Di Gioacchino <alessandro.digioacchino@studenti.unimi.it>
2026-08-16 00:22:42 +0200

    Cambia rendering valute

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ app = Flask( __name__ )
 def index():
   risultato = None
   errore = None
-  ral_default = '35000'
+  ral_default = "35'000,00"
   mensilita_default = 13
 
   if request.method == 'POST':
     try:
-      ral_input = request.form.get( 'ral', '' ).replace( ',', '.' ).strip()
+      ral_input = request.form.get( 'ral', '' ).strip()
       mensilita_input = int( request.form.get( 'mensilita', 13 ) )
 
       if not ral_input:
         errore = 'inserisci una Retribuzione Annua Lorda (RAL) valida.'
       else:
-        ral = float( ral_input )
+        ral = valuta2float( ral_input )
         if ral < 0:
-          error = 'La Retribuzione Annua Lorda (RAL) deve essere un valore' \
-                  'positivo.'
+          errore = 'La Retribuzione Annua Lorda (RAL) deve essere un valore ' \
+                   'positivo.'
 
         else:
           ral_default = ral
@@ -31,12 +31,34 @@ def index():
           csi = CalcoloStipendioItaliano( ral )
           risultato = csi.calcola( mensilita_input )
     except ValueError:
-      errore = 'Formato numerico non valido. Esempi: 35000 o 35000.00'
+      errore = "Formato numerico non valido. Esempi: 35'000,00 o 35000"
+
+  ral_default = filtro_valuta( ral_default )
 
   return render_template(
     'index.html', risultato=risultato, errore=errore, ral_default=ral_default,
     mensilita_default=mensilita_default
   )
+
+@app.template_filter( 'valuta' )
+def filtro_valuta( val: float ) -> str:
+  if val is None:
+    return '0,00'
+
+  try:
+    formattato = f'{float( val ):,.2f}'
+    return formattato.replace( ',', "'" ).replace( '.', ',' )
+  except ( ValueError, TypeError ):
+    return str( val )
+
+def valuta2float( val_str: str ) -> float:
+  if not val_str:
+    return .0
+
+  pulita = val_str.replace( "'", '' ).replace( ' ', '' ).replace( '.', '' )
+  pulita = pulita.replace( ',', '.' )
+
+  return float( pulita )
 
 
 if __name__ == '__main__':
